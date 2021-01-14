78176a5b5026f26d2cbeb264c324df23970bfade
fakhrurrozi-dev <77405059+fakhrurrozi-dev@users.noreply.github.com>
2021-01-14 07:02:18 +0700

    Add files via upload

---
+++ contoh_1.py
@@ -0,0 +1,6 @@
+data_string = "Fakhrurrozi"
+data_integer = "23"
+data_float = "1.70"
+print( "Nama saya adalah : ",data_string) 
+print("Saya berumur : ",data_integer )
+print("Tinggi saya adalah : ",data_float)
