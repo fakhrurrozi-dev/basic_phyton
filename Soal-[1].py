6b35b43fe5bb049ca2c59f59bfbabb7466785c98
fakhrurrozi-dev <77405059+fakhrurrozi-dev@users.noreply.github.com>
2021-01-16 07:36:13 +0700

    Update Soal-[1].py

--- Soal-[1].py
+++ Soal-[1].py
@@ -1,11 +1,4 @@
 data_string = "Fakhrurrozi"
 data_integer = "23"
 data_float = "1.70"
-txt1 = "Nama Saya {}".format(data_string)
-txt2 = ","
-txt3 = " umur saya {}".format(data_integer)
-txt4 =  " tahun"
-txt5 = " dan tinggi saya {}".format(data_float)
-txt6 =  "m ."
-txt = txt1 + txt2 +txt3 + txt4 + txt5 + txt6
-print(txt)
+print('Nama saya',data_string,'umur saya',data_integer,'tahun dan tinggi saya',data_float,'cm.')
